4f70312aa3e3805235003a937200713902f34383
yyconn <conn@conn.jp>
2015-04-10 10:12:27 +0900

    pb-cli now removes all NL characters from all strings of output generated with --extra/trace

--- pb-cli.js
+++ pb-cli.js
@@ -299,6 +299,22 @@ var pullResp = function (error, response, body) {
     }
 }
 
+var mapAll = function (jObj, func) {
+    for (var i in jObj) {
+	jObj[i] = func.call(this, jObj[i]);
+	if (jObj[i] !== null && typeof (jObj[i]) == 'object')
+	    jObj[i] = mapAll(jObj[i], func);
+    }
+    return jObj;
+}
+
+var removeNewLine = function (obj) {
+    if (typeof obj == 'string')
+	return obj.replace(/\n/g, '');
+    else
+	return obj;
+}
+
 var talkResp = function (error, response, body) {
     if (!response)
 	console.log(error);
@@ -308,20 +324,8 @@ var talkResp = function (error, response, body) {
 	var jObj = JSON.parse(body);
 	if (jObj.status === 'ok') {
 	    nconf.set('sessionid', jObj.sessionid);
-	    if (nconf.get('extra')) {
-		jObj.templates = jObj.templates.map (function (str) {
-		    return str.replace(/\n/g, '');
-		});
-		console.log(JSON.stringify(jObj, null, 2));
-	    }
-	    else if (nconf.get('trace')) {
-		jObj.trace = jObj.trace.map (function (obj) {
-		    if (obj.template)
-			obj.template = obj.template.replace(/\n/g, '');
-		    return obj;
-		});
-		console.log(JSON.stringify(jObj, null, 2));
-	    }
+	    if (nconf.get('extra') || nconf.get('trace'))
+		console.log(JSON.stringify(mapAll(jObj, removeNewLine), null, 2));
 	    else {
 		jObj.responses.forEach (function (entry) {
 		    console.log(entry);
